917933d8d10893c0afe578e7d8ebd0bc8735b85d
Pinar <kubraa__31@hotmail.com>
2018-02-01 15:05:49 +0100

    bestandInlezen

---
+++ VirusHostDB/src/virushostdb/Noggeennaam.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virushostdb;
+
+/**
+ *
+ * @author Pinar Kubra Kumurcu
+ * Bi6a-T Eindopdracht informatica ViralHostDB
+ */
+public class Noggeennaam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
